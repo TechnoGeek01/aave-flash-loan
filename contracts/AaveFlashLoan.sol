1f60f5aac34873e6b46a7ec65bfba9dcf20da5cc
TechnoGeek01 <rsudharsangames@gmail.com>
2023-08-03 18:20:12 +0530

    aave flash loan contract

---
+++ contracts/AaveFlashLoan.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import {FlashLoanSimpleReceiverBase} from "@aave/core-v3/contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol";
+import {IPoolAddressesProvider} from "@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol";
+import {IERC20} from "@aave/core-v3/contracts/dependencies/openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract AaveFlashLoan is FlashLoanSimpleReceiverBase {
+    error NotOwner();
+
+    address payable owner;
+
+    constructor(
+        address _addressProvider
+    ) FlashLoanSimpleReceiverBase(IPoolAddressesProvider(_addressProvider)) {
+        owner = payable(msg.sender);
+    }
+
+    modifier onlyOwner {
+        if (msg.sender!=owner) {
+            revert NotOwner();
+        }
+        _;
+    }
+
+    function executeOperation(
+        address asset,
+        uint256 amount,
+        uint256 premium,
+        address initiator,
+        bytes calldata params
+    ) external override returns (bool) {
+        // we have the borrowed funds
+        // custom logic
+
+        uint amountOwed = amount + premium;
+        IERC20(asset).approve(address(POOL), amountOwed);
+
+        return true;
+    }
+
+    function requestFlashLoan(address _token, uint _amount) public {
+        address receiverAddress = address(this);
+        address asset = _token;
+        uint amount = _amount;
+        bytes memory params = "";
+        uint16 referralCode = 0;
+
+        POOL.flashLoanSimple(
+            receiverAddress,
+            asset,
+            amount,
+            params,
+            referralCode
+        );
+    }
+
+    function getBalance(address _tokenAddress) public view returns (uint) {
+        return IERC20(_tokenAddress).balanceOf(address(this));
+    }
+
+    function withdraw(address _tokenAddress) external onlyOwner{
+        IERC20 token=IERC20(_tokenAddress)
+        token.transfer(msg.sender, token.balanceOf(address(this)));
+    }
+
+    receive() external payable{}
+
+}
